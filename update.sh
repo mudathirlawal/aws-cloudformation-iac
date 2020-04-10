cc028d0554db4d524035666ab707164179f9d139
Mudathir Lawal <nigercode@gmail.com>
2020-04-10 15:57:28 +0100

    remove bugs from update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-aws cloudformation update-stack $1 \
---stack-name $2 \
---template-body file file://$2 \
+aws cloudformation update-stack \
+--stack-name $1 \
+--template-body file://$2 \
 --parameters file://$3 \
 region=us-west-2
